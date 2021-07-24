85d75d7f1e0d6428ce08462e9aed03e8d4da74c8
Darren-Nicol <darren@nudgeexchange.com>
2021-07-24 06:25:03 +0100

    strat work on load mailbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -31,6 +31,9 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#single-email-view').style.display = 'none';
+  document.querySelectorAll("button").forEach(button => button.classList.remove("selected"));
+  document.querySelector(`#${mailbox}`).classList.add("selected");
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
